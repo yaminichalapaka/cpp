192c13b570830d3ee74186322918640f482dadc2
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2023-06-21 18:57:48 +0530

    Create trainglearea.cpp

---
+++ trainglearea.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float area,h,b;
+    cout<<"Enter the height and base: ";
+    cin>>h>>b;
+    area=(h*b)/2;
+    printf("%0.2f",area);
+    return 0;
+}
