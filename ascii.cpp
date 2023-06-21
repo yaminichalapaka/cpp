9acd1249a56f18b274015de46b9e99bec52d5c50
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2023-06-21 18:44:13 +0530

    Create ascii.cpp

---
+++ ascii.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c;
+cout<<"Enter a character: ";
+cin>>c;
+cout<<"The ASCII value of inserted character is "<<(int)c;
+return 0;
+}
