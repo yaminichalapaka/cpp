e60ba7b63fee8356bbec0e7df04e743fe8394308
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2023-06-21 18:55:06 +0530

    Create rectanglearea.cpp

---
+++ rectanglearea.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float area,l,b;
+    cout<<"Enter the length and breadth: ";
+    cin>>l>>b;
+    area=l*b;
+    printf("%0.2f",area);
+    return 0;
+}
